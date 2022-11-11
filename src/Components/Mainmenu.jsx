82d301145e48a4788acd443223e05033f8cf3bee
Obaid Ur Rehman <obaidurrehman8121@gmail.com>
2022-11-11 13:31:42 +0200

    ready

---
+++ src/Components/Mainmenu.jsx
@@ -0,0 +1,23 @@
+import React ,{useContext}from 'react';
+import { GameStateContext } from '../App';
+
+
+const MainMenu = () => {
+    const {game,setgame,myname,setmyname}=useContext(GameStateContext);
+
+  return (
+    <>
+    <div className="Menu">
+      <input type="text" placeholder='Enter your Name' onChange={(e)=>{
+        setmyname(e.target.value)
+      }} />
+      <button onClick={()=>{
+        setgame("quiz");
+      }}>Start Quiz</button>
+    </div>
+    </>
+  )
+}
+
+
+export default MainMenu;
---
+++ src/Components/Questions.js
@@ -0,0 +1,71 @@
+export const Questions = [
+    {
+      prompt: "What is the Capital Of Pakistan?",
+      optionA: "Quetta",
+      optionB: "Karachi",
+      optionC: "Islamabad",
+      optionD: "Lahore",
+      asnwer: "C",
+    },
+    {
+      prompt: "What does CSS stand for?",
+      optionA: "Computing Style Sheet",
+      optionB: "Creative Style System",
+      optionC: "Cascading Style Sheet",
+      optionD: "Creative Styling Sheet",
+      asnwer: "C",
+    },
+    {
+      prompt: "Which one is the smallest heading tag in HTML?",
+      optionA: "h3",
+      optionB: "h1",
+      optionC: "h4",
+      optionD: "h5",
+      asnwer: "D",
+    },
+    {
+      prompt: "Which of this is not a programming language?",
+      optionA: "Python",
+      optionB: "JavaScript",
+      optionC: "MC-03",
+      optionD: "Java",
+      asnwer: "C",
+    },
+    {
+      prompt: "Which of this is not a javascript framework?",
+      optionA: "React",
+      optionB: "Angular",
+      optionC: "Vue",
+      optionD: "Java",
+      asnwer: "D",
+    },
+  ];
+
+
+// export const quizData = [
+//     {
+//       id: 0,
+//       question: ` What is the Capital Of Pakistan ?`,
+//       options: [`New Delhi`, `Mumbai`, `Kolkatta`],
+//       answer: `New Delhi`
+//     },
+//     {
+//       id: 1,
+//       question: `Who is the CEO of Tesla Motors?`,
+//       options: [`Bill Gates`, `Steve Jobs`, `Elon Musk`],
+//       answer: `Elon Musk`
+//     },
+//     {
+//       id: 3,
+//       question: `Name World's Richest Man?`,
+//       options: [`Jeff Bezo`, `Bill Gates`, `Mark Zuckerberg`],
+//       answer: `Jeff Bezo`
+//     },
+//     {
+//       id: 4,
+//       question: `World's Longest River?`,
+//       options: [`River Nile`, `River Amazon`, `River Godavari`],
+//       answer: `River Nile`
+//     }
+//   ];
+  
